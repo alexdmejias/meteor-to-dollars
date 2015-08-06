e7383b2d40df4975083e9af041f5d7f2316042f7
Alex Mejias <alexdmejias@gmail.com>
2015-08-06 13:49:23 -0400

    updated package desc

--- package.js
+++ package.js
@@ -2,16 +2,16 @@ Package.describe({
   name: 'alexdmejias:to-dollars',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Easily format number objects to dollars',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/amejias101/meteor-to-dollars',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.2');
+  api.versionsFrom('1.0');
   api.addFiles('to-dollars.js');
 });
 
